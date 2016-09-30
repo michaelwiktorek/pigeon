e288e048373e213e315d955e4c4beb6272ce5ccc
Michael Wiktorek <wiktorekmichael@gmail.com>
2016-09-30 13:56:57 -0700

    first working version

--- pigeon.py
+++ pigeon.py
@@ -1,18 +1,23 @@
 import threading
 import socket
 import time
+import select
 import curses
 import curses.textpad
 from Queue import Queue
 
 class GUI:
-    def __init__(self):
+    def __init__(self, sock):
         self.msg_send = Queue()
-       
-    def start(self):
-        curses.wrapper(self.operate)
+        self.sock = sock
+        self.KILL_MSG = "*>*>*>*>*>"
+        self.ALIVE = False
 
-    def operate(self, screen):
+    def start(self):
+        curses.wrapper(self.initialize)
+       
+    def initialize(self, screen):
+        self.ALIVE = True
         scr_y, scr_x = screen.getmaxyx()
         # we display chat history in the display_pad
         self.display_pad = curses.newpad(scr_y-1, scr_x)
@@ -20,26 +25,37 @@ class GUI:
         self.display_pad.scrollok(True)
         self.scroll = 0
         # we display the message you type in the text window
-        text_window = curses.newwin(1, scr_x, scr_y-1, 0)
-        textbox = curses.textpad.Textbox(text_window, insert_mode=True)
-        
+        self.text_window = curses.newwin(1, scr_x, scr_y-1, 0)
+        self.textbox = curses.textpad.Textbox(self.text_window, insert_mode=True)
+
+        sender = threading.Thread(target=send_worker, args=(self.sock, self))
+        receiver = threading.Thread(target=receive_worker, args=(self.sock, self))
+        sender.start()
+        receiver.start()
+
+        self.function_loop(screen)
+        self.ALIVE = False
+        sender.join()
+        receiver.join()
+
+    def function_loop(self, screen):  
         #loop accepting input and echoing to the display_pad
-        while True:
-            message = textbox.edit()
+        while self.ALIVE:
+            message = self.textbox.edit()
             # put message into send queue
-            self.msg_send.put(message, block=True, timeout=5)
+            self.msg_send.put(message)
             # for whatever reason only the empty string matches correctly?
             if message == "":
-                break
-            text_window.clear()
-            text_window.move(0,0)
+                self.msg_send.put(self.KILL_MSG)
+            self.text_window.clear()
+            self.text_window.move(0,0)
             self.display_message(message, "You")
 
     def display_message(self, message, name):
         pad_y, pad_x =self.display_pad.getmaxyx()
         curs_y, curs_x = self.display_pad.getyx()
         self.display_pad.addstr(name + ":  " + message)
-        self.display_pad.addstr(str(curs_y) + "/" + str(pad_y))
+        #self.display_pad.addstr(str(curs_y) + "/" + str(pad_y))
         
         if curs_y == pad_y - 1:
             self.scroll = self.scroll + 1
@@ -53,10 +69,10 @@ class Communicator:
     def __init__(self):
         print "Pigeon is starting"
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.PORT = 1776
+        self.PORT = 1777
         self.HOST = ""
         self.AM_SERVER = False
-        self.gui = GUI()
+        self.TIMEOUT = 1
 
     def start(self):
         instruction = raw_input("Wait for a connection, or attempt to Connect (w/c): ")
@@ -65,57 +81,45 @@ class Communicator:
             self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             self.sock.bind((self.HOST, self.PORT))
             self.sock.listen(1)
-            self.conn, self.addr = self.sock.accept() # here's where we wait
-            sender = threading.Thread(target=send_worker, args=(self.conn, self.gui))
-            receiver = threading.Thread(target=receive_worker, args=(self.conn, self.gui))
+            conn, addr = self.sock.accept() # here's where we wait
+            conn.settimeout(self.TIMEOUT)
+            self.op_socket = conn
         else:
             ip = raw_input("Enter ip address: ")
             self.sock.connect((ip, self.PORT))
-            # we have now established a connection
-            # we can send and receive on this connection
-            sender = threading.Thread(target=send_worker, args=(self.sock, self.gui))
-            receiver = threading.Thread(target=receive_worker, args=(self.sock, self.gui))
-            
+            self.sock.settimeout(self.TIMEOUT)
+            self.op_socket = self.sock
+
+        self.gui = GUI(self.op_socket)
         self.gui.start()
-        sender.start()
-        receiver.start()
-        sender.join()
-        self.sock.close()
-        receiver.join()
 
         print "pigeon has experienced the sweet release of death"
 
 
 def send_worker(sock, gui):
-    running = True
-    while running == True:
+    while gui.ALIVE:
         try:
             message = gui.msg_send.get(block=True, timeout=5)
             sock.sendall(message)
-            if message == "":
-                running = False
+            if message == gui.KILL_MSG:
+                gui.ALIVE = False
         except:
             continue # we can do something here maybe
 
 
 def receive_worker(sock, gui):
-    running = True
-    while running == True:
-        message = sock.recv(1024)
-        if message:
-            gui.display_message(message, "Them")
-        else:
-            running = False
-                
+    while gui.ALIVE:
+        try:
+            message = sock.recv(1024)
+            if message == gui.KILL_MSG:
+                gui.display_message("Other side has disconnected, [ENTER] to quit", "**SYSTEM**")
+                gui.ALIVE = False
+            elif message:
+                gui.display_message(message, "Them")
+        except:
+            continue
+            
         
 if __name__ == '__main__':
-    # start by asking the user whether to oh god none of this works
-    # we go full P2P
-    # start program
-    # option to send connect request
-    # if request received, then spin up sender thread, connect to origin
-
-    # all garbage, see ideas.txt
-    
     comm = Communicator()
     comm.start()
