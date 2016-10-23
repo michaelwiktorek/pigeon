b9510bdec001bf14f21282441005bafa4efe802b
Michael Wiktorek <wiktorekmichael@gmail.com>
2016-10-22 19:24:27 -0700

    minor server changes

--- server.py
+++ server.py
@@ -17,11 +17,9 @@ class Pigeon_Server:
 
     def start(self):
         self.ALIVE = True
-        #self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.settimeout(self.TIMEOUT)
         self.sock.bind((self.HOST, C.SERVER_MAIN_PORT))
-        #self.sock.listen(5)
         print "listening on port " + str(C.SERVER_MAIN_PORT)
 
         # here we need to start our user-pruning thread
@@ -40,11 +38,10 @@ class Pigeon_Server:
     def prune_users(self):
         while self.ALIVE:
             self.userlist_lock.acquire()
-            #print "pruning offline users"
             prune_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            # bind to server main port (is this allowed?)  NO
+
             prune_socket.bind((self.HOST, C.SERVER_REGISTER_PORT))
-            prune_socket.settimeout(2) # allow 2 secs for a response
+            prune_socket.settimeout(1) # allow 1 sec for response (constant?)
             # THIS COULD BE VERY WEIRD WITH THREADS, LOOK INTO IT
             # mark unresponsive clients for deletion
             for addr in self.online_users.keys():
@@ -52,14 +49,10 @@ class Pigeon_Server:
                 try:
                     mesg, addr = prune_socket.recvfrom(1024)
                     if mesg == C.ACK:
-                        # update time # actually don't
-                        #self.online_users[addr][1] = time.time()
-                        #print self.online_users[addr][0] + " is still online"
                         continue
                 except:
                     self.online_users[addr] = None
                     
-            # close socket
             prune_socket.close()
 
             # remove unresponsive clients from list
@@ -95,7 +88,7 @@ class Pigeon_Server:
                 self.online_users[client_addr] = name, time.time()
                 self.send_ack(addr)
             elif mesg == C.UNREGISTER:
-                print "UNregister from " + name + " at " + client_addr
+                print "un_register from " + name + " at " + client_addr
                 self.send_ack(addr)
                 try:
                     del self.online_users[client_addr]
@@ -112,9 +105,6 @@ class Pigeon_Server:
             self.userlist_lock.release()
             
     def send_ack(self, address):
-        print ""
-        print "sending ack to " + address[0] + ":" + str(address[1])
-        print ""
         ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         ack_sock.sendto(C.ACK, (address[0], address[1]))
         ack_sock.close()
@@ -125,11 +115,11 @@ if __name__ == "__main__":
     server = Pigeon_Server()
 
     def signal_handler(*args):
-        print "Attempting to kill Pigeon server...."
+        print "Quitting Pigeon Server....."
         server.ALIVE = False
 
     signal.signal(signal.SIGINT, signal_handler)
     
     server.start()
 
-    print "Prune thread joined, main thread dying now!"
+    print "Goodbye!"
