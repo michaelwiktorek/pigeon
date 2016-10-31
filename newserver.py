a098ca6fc8217c58323cbcea331269b40075eff2
Michael Wiktorek <wiktorekmichael@gmail.com>
2016-10-31 15:26:06 -0700

    testing new server

---
+++ newserver.py
@@ -0,0 +1,125 @@
+"""
+Pigeon is a peer-to-peer chat client with end-to-end encryption.
+Copyright (C) 2016 Michael Wiktorek
+"""
+
+# This server should probably only be run on a POSIX-compliant system
+# otherwise some of the socket stuff might not work
+
+import socket
+import select
+import threading
+import signal
+import json
+from pigeon_constants import Pigeon_Constants as C
+
+class Pigeon_Server:
+    def __init__(self, port=None):
+        # dict maps username => ip address
+        self.online_users = {}
+        self.connections = []
+        self.userlist_lock = threading.Lock()
+        self.connections_lock = threading.Lock()
+        self.ALIVE = False
+        self.TIMEOUT = 1
+        self.HOST = ""
+
+    def start(self):
+        self.ALIVE = True
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.settimeout(self.TIMEOUT)
+        self.server.bind((self.HOST, C.SERVER_MAIN_PORT))
+        self.server.listen(5)
+        print "listening on port " + str(C.SERVER_MAIN_PORT)
+
+        # here is our main server loop
+        self.connect_loop()
+
+        # TODO this may be unnecessary
+        self.server.close()
+
+    def connect_loop(self):
+        while self.ALIVE:
+            try:
+                reads, writes, errs = select.select(self.connections, [], self.connections)
+                for conn in reads:
+                    self.handle_read(conn)
+                for conn in errs:
+                    self.handle_err(conn)
+            except:
+                self.kill_connections()
+                print "Select call interrupted (this is probably okay?)"
+            
+    def handle_read(self, conn):
+        if conn == self.server:
+            self.handle_server(conn)
+        else:
+            self.handle_message(conn)
+            
+    def handle_message(self, conn):
+        try:
+            data = conn.recv(2048)
+        except:
+            print "Error reading from " + conn.getpeername()[0]
+            return
+        user_info = data.split(":")
+        name = user_info[0]
+        mesg = user_info[1]
+        client_addr = conn.getpeername()[0]
+        # update online-users list
+        if mesg == C.REGISTER:
+            print "register from " + name + " at " + client_addr
+            self.online_users[client_addr] = name
+        elif mesg == C.UNREGISTER:
+            print "un_register from " + name + " at " + client_addr
+            try:
+                del self.online_users[client_addr]
+            except KeyError:
+                print name + " was already offline!"
+            conn.close()
+            self.connections.remove(conn)
+        elif mesg == C.REQUEST:
+            userlist = json.dumps(self.online_users)
+            conn.sendall(userlist)
+
+    def handle_server(self, conn):
+        try:
+            conn, addr = self.sock.accept()
+        except:
+            print "Error receiving new connection"
+            return
+        self.connections.append()
+
+    def handle_err(self, conn):
+        client_addr = conn.getpeername()[0]
+        print client_addr + " produced error (lost connection?)"
+        try:
+            del self.online_users[client_addr]
+        except:
+            print client_addr + " errored but was already offline"
+        conn.close()
+        self.connections.remove(conn)
+
+    def kill_connections(self):
+        for conn in self.connections:
+            conn.close()
+
+    def print_online_users(self):
+        print str(len(self.online_users)) + " Users Online: "
+        for addr in self.online_users.keys():
+            print self.online_users[addr][0] + " at " + addr
+
+                    
+            
+if __name__ == "__main__":
+    server = Pigeon_Server()
+
+    def signal_handler(*args):
+        print "Quitting Pigeon Server....."
+        server.ALIVE = False
+
+    signal.signal(signal.SIGINT, signal_handler)
+    
+    server.start()
+
+    print "Goodbye!"
--- pigeon_register_agent.py
+++ pigeon_register_agent.py
@@ -17,89 +17,68 @@ class Pigeon_Register_Agent:
         self.HOST = ""
         self.server_ip = C.DEFAULT_SERVER
         self.MAX_ATTEMPTS = 1
-        self.ALIVE = True
+        self.TIMEOUT = 1
 
-    def set_server_ip(self, ip):
-        self.server_ip = ip
-
-    def get_gui(self, gui):
-        self.gui = gui
-        self.display = gui.system_pad
-        self.textbox = gui.textbox
+    def establish_conn(self):
+        try:
+            self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.conn.settimeout(self.TIMEOUT)
+            self.conn.connect((self.server_ip, C.SERVER_MAIN_PORT))
+            return True
+        except:
+            return False
         
-    def send_wait_ack(self, message):
-        send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # send message, await ack and resend message until ack
-        send_sock.bind((self.HOST, C.CLIENT_REGISTER_PORT))
-        send_sock.sendto(message, (self.server_ip, C.SERVER_MAIN_PORT))
-        send_sock.settimeout(1)
+    def close_conn(self):
+        self.conn.close()
+
+    def send_message(self, message):
+        try:
+            self.conn.sendall(message)
+            return True
+        except:
+            return False
+
+    def get_response(self):
         attempts_made = 0
         while attempts_made < self.MAX_ATTEMPTS:
             try:
-                attempts_made = attempts_made + 1
-                mesg, addr = send_sock.recvfrom(1024)
-                if mesg == C.ACK:
-                    send_sock.close()
-                    attempts_made = 0
-                    return True
-                else:
-                    send_sock.close()
-                    attempts_made = 0
-                    return mesg
+                return self.conn.recv(2048)
             except:
                 continue
-        send_sock.close()
-        attempts_made = 0
-        return False
-
-    def keep_alive(self):
-        test_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        test_sock.bind((self.HOST, C.CLIENT_TEST_PORT))
-        test_sock.settimeout(1)
-        while self.ALIVE:
-            try:
-                mesg, addr = test_sock.recvfrom(1024)
-                if mesg == C.TEST:
-                    test_sock.sendto(C.ACK, (addr[0], addr[1]))
-            except:
-                continue
-        test_sock.close()
+        return None
 
     def request_userlist(self, name):
         # return deserialized json userlist
-        mesg = self.send_wait_ack(name + ":" + C.REQUEST)
-        if mesg:
-            self.userlist = json.loads(mesg)
-            return True
-        else:
-            return False
+        if self.send_message(name + ":" + C.REQUEST):
+            response = self.get_response()
+            if response:
+                self.userlist = json.loads(response)
+                return True
+        return False
 
     def print_userlist(self):
         for addr in self.userlist.keys():
             print self.userlist[addr][0] + " online at " + addr
-        
-    # unregister and re-register without killing keepalive thread
+
+    
     def re_register(self, old_name, new_name):
-        self.send_wait_ack(old_name + ":" + C.UNREGISTER)
-        self.send_wait_ack(new_name + ":" + C.REGISTER)
+        self.send_message(old_name + ":" + C.UNREGISTER)
+        self.send_message(new_name + ":" + C.REGISTER)
 
 
     def register(self, name):
-        if self.send_wait_ack(name + ":" + C.REGISTER):
-            self.ALIVE = True
+        if self.establish_conn() and self.send_message(name + ":" + C.REGISTER):
             self.CONNECTED = True
-            self.keep_alive_thread = threading.Thread(target=self.keep_alive)
-            self.keep_alive_thread.start()
             return True
         else:
             return False
 
     def unregister(self, name):
-        self.ALIVE = False
         # only send an unregister message if we registered successfully
         if self.CONNECTED:
             self.CONNECTED = False
-            return self.send_wait_ack(name + ":" + C.UNREGISTER)
+            self.send_message(name + ":" + C.UNREGISTER)
+            self.close_conn()
         else:
             return False
         
