d858242b0af81a34f6f5c11b4a60475f66dd2d71
Michael Wiktorek <wiktorekmichael@gmail.com>
2016-10-31 17:08:12 -0700

    new server working properly

--- server.py
+++ server.py
@@ -3,11 +3,12 @@ Pigeon is a peer-to-peer chat client with end-to-end encryption.
 Copyright (C) 2016 Michael Wiktorek
 """
 
+# This server might not run properly on Windows
+
 import socket
+import select
 import threading
 import signal
-import sys
-import time
 import json
 from pigeon_constants import Pigeon_Constants as C
 
@@ -15,104 +16,111 @@ class Pigeon_Server:
     def __init__(self, port=None):
         # dict maps username => ip address
         self.online_users = {}
+        self.connections = []
         self.userlist_lock = threading.Lock()
+        self.connections_lock = threading.Lock()
         self.ALIVE = False
         self.TIMEOUT = 1
         self.HOST = ""
 
+    # init listen socket and start main loop
     def start(self):
         self.ALIVE = True
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.settimeout(self.TIMEOUT)
-        self.sock.bind((self.HOST, C.SERVER_MAIN_PORT))
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.server.settimeout(self.TIMEOUT)
+        self.server.bind((self.HOST, C.SERVER_MAIN_PORT))
+        self.server.setblocking(0)
+        self.server.listen(5)
+        self.connections.append(self.server)
         print "listening on port " + str(C.SERVER_MAIN_PORT)
 
-        # here we need to start our user-pruning thread
-        pruning_thread = threading.Thread(target=self.prune_users)
-        pruning_thread.start()
-        
-        self.recv_loop_UDP()
-        self.sock.close()
-        pruning_thread.join()
+        # here is our main server loop
+        self.connect_loop()
 
+        # close all connections when server terminates
+        self.kill_connections()
+
+    # loop on select on all open sockets
+    def connect_loop(self):
+        while self.ALIVE:
+            try:
+                reads, writes, errs = select.select(self.connections, [], [])
+                for conn in reads:
+                    self.handle_read(conn)
+            except select.error:
+                print "Interrupted system call 'select'. This is normal."
+
+    # sort out new connections from data packets
+    def handle_read(self, conn):
+        if conn is self.server:
+            self.handle_server(conn)
+        else:
+            self.handle_data(conn)
+
+    # sort out filled data packets from empties (indicate disconnect)
+    def handle_data(self, conn):
+        data = conn.recv(2048)
+        if data:
+            self.handle_message(conn, data)
+        else:
+            self.handle_disconnect(conn)
+
+    # handle empty packet (disconnect)
+    def handle_disconnect(self, conn):
+        client_addr = conn.getpeername()[0]
+        print client_addr + " suddenly disconnected"
+        try:
+            del self.online_users[client_addr]
+        except:
+            print client_addr + " disconnected but was already offline... ?"
+        conn.close()
+        self.connections.remove(conn)
+
+    # handle filled data packet (message from client)
+    def handle_message(self, conn, data):
+        user_info = data.split(":")
+        name = user_info[0]
+        mesg = user_info[1]
+        client_addr = conn.getpeername()[0]
+        # update online-users list
+        if mesg == C.REGISTER:
+            print "register from " + name + " at " + client_addr
+            self.online_users[client_addr] = name
+        elif mesg == C.RE_REGISTER:
+            try:
+                self.online_users[client_addr] = name
+            except KeyError:
+                print "Invalid rename from " + client_addr
+        elif mesg == C.UNREGISTER:
+            print "un_register from " + name + " at " + client_addr
+            try:
+                del self.online_users[client_addr]
+            except KeyError:
+                print name + " was already offline... ?"
+            conn.close()
+            self.connections.remove(conn)
+        elif mesg == C.REQUEST:
+            print "Sending userlist to " + name + " at " + client_addr
+            userlist = json.dumps(self.online_users)
+            conn.sendall(userlist)
+
+    # handle connection request from a new client
+    def handle_server(self, conn):
+        new_conn, addr = conn.accept()
+        print "Received initial connection from " + addr[0]
+        new_conn.setblocking(0)
+        self.connections.append(new_conn)
+
+    # TODO: consider notifying clients of server's termination
+    def kill_connections(self):
+        for conn in self.connections:
+            conn.close()
+
+    # this is for debug purposes
     def print_online_users(self):
         print str(len(self.online_users)) + " Users Online: "
         for addr in self.online_users.keys():
             print self.online_users[addr][0] + " at " + addr
-
-    def prune_users(self):
-        while self.ALIVE:
-            self.userlist_lock.acquire()
-            prune_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-            prune_socket.bind((self.HOST, C.SERVER_REGISTER_PORT))
-            prune_socket.settimeout(1) # allow 1 sec for response (constant?)
-            # THIS COULD BE VERY WEIRD WITH THREADS, LOOK INTO IT
-            # mark unresponsive clients for deletion
-            for addr in self.online_users.keys():
-                prune_socket.sendto(C.TEST, (addr, C.CLIENT_TEST_PORT))
-                try:
-                    mesg, addr = prune_socket.recvfrom(1024)
-                    if mesg == C.ACK:
-                        continue
-                except:
-                    self.online_users[addr] = None
-                    
-            prune_socket.close()
-
-            # remove unresponsive clients from list
-            for addr in self.online_users.keys():
-                if self.online_users[addr] is None:
-                    del self.online_users[addr]
-
-            self.userlist_lock.release()
-            
-            # is this idiotic? I think so
-            for i in range(C.ONLINE_TIMEOUT_SECS):
-                if not self.ALIVE:
-                    break  # let me die
-                time.sleep(1)
-
-        #print ("...")
-
-    def recv_loop_UDP(self):
-        while self.ALIVE:
-            try:
-                data, addr = self.sock.recvfrom(1024)
-            except:
-                continue
-            client_port = addr[1]
-            client_addr = addr[0]
-            user_info = data.split(":")
-            name = user_info[0]
-            mesg = user_info[1]
-            # update online-users list
-            self.userlist_lock.acquire()
-            if mesg == C.REGISTER:
-                print "register from " + name + " at " + client_addr
-                self.online_users[client_addr] = name, time.time()
-                self.send_ack(addr)
-            elif mesg == C.UNREGISTER:
-                print "un_register from " + name + " at " + client_addr
-                self.send_ack(addr)
-                try:
-                    del self.online_users[client_addr]
-                except KeyError:
-                    print "User is already offline!"
-            elif mesg == C.REQUEST:
-                # serialize and send userlist to client
-                userlist = json.dumps(self.online_users)
-                list_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                list_sock.sendto(userlist, (client_addr, client_port))
-                list_sock.close()
-            
-            #self.print_online_users()
-            self.userlist_lock.release()
-            
-    def send_ack(self, address):
-        ack_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        ack_sock.sendto(C.ACK, (address[0], address[1]))
-        ack_sock.close()
 
                     
             
